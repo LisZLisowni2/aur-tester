da26dd5fc52596b5d6029fcdc0535b61c05e7e00
LisZLisowni2 <rafalkozikowski735@gmail.com>
2024-12-23 11:55:24 +0100

    Change the script file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM archlinux:latest
-RUN pacman -Syu --noconfirm && pacman -S --noconfirm base-devel git
-WORKDIR /aur-package
-ENTRYPOINT ['/bin/bash']
+RUN pacman -Syu --noconfirm && pacman -S --noconfirm bash base-devel git shellcheck clamav rkhunter && useradd -m buildtest && echo "buildtest ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+WORKDIR /home/buildtest/aur-package
+RUN chown -R buildtest /home/buildtest/aur-package
+ENTRYPOINT ["/bin/bash"]
--- aur-tester.sh
+++ aur-tester.sh
@@ -2,9 +2,11 @@
 
 # Default values
 OUTPUT_DIR="./aur-packages"
+IMAGE_NAME="aur-tester-image"
 KEEP_CONTAINER=false
 VERBOSE=false
 VERSION="1.0.0"
+SCANALL=true
 
 # Help message
 usage() {
@@ -16,6 +18,7 @@ usage() {
     echo "  -o, --output <dir>    Specify the output directory for built packages (default: $OUTPUT_DIR)"
     echo "  -k, --keep-container  Keep the Docker container after execution (default: false)"
     echo "  -v, --verbose         Enable verbose output (default: false)"
+    echo "  -s, --minimal-scan    Scan only changed files, not all system"
     echo "  --version             Show the script version and exit"
     exit 0
 }
@@ -25,15 +28,23 @@ while [[ "$#" -gt 0 ]]; do
     case "$1" in
         -h|--help)
             usage
+            exit 0
             ;;
         -o|--o)
             OUTPUT_DIR="$2"
+            shift 2
             ;;
         -k|--keep-container)
             KEEP_CONTAINER=true
+            shift
             ;;
         -v|--verbose)
             VERBOSE=true  
+            shift
+            ;;
+        -s|--minimal-scan)
+            SCANALL=false
+            shift
             ;;
         --version)
             echo "aur-tester version $VERSION"
@@ -42,10 +53,13 @@ while [[ "$#" -gt 0 ]]; do
         -*)
             echo "Unknown option: $1" >&2
             usage
+            exit 1
             ;;
         *)
             PACKAGE_NAME="$1"
+            shift
             ;;
+    esac
 done
 
 if [ -z "$PACKAGE_NAME" ]; then
@@ -59,10 +73,67 @@ log() {
     fi
 }
 
+test_docker_service() {
+    if ! systemctl is-active --quiet docker; then
+        sudo systemctl start docker
+    fi
+}
+
+run_docker_and_test() {
+    docker run --rm -it --name $PACKAGE_NAME-testing --user root aur-tester-image:$VERSION -c "
+        set -ex &&
+        echo \"Clone a git repo of $PACKAGE_NAME and build it\" &&
+        find $pkgdir -type f > /tmp/before_install.txt &&
+        su buildtest -c \"
+            echo \"Clone\" &&
+            git clone https://aur.archlinux.org/$PACKAGE_NAME.git && 
+            cd $PACKAGE_NAME &&
+            echo \"Sums\" &&
+            sha256sum PKGBUILD
+            if [ \$? -ne 0 ]; then
+                echo \"sha256sum failed, continuing...\"
+            fi &&
+            echo \"Code\" &&
+            shellcheck PKGBUILD
+            if [ \$? -ne 0 ]; then
+                echo \"shellcheck failed, continuing...\"
+            fi &&
+            echo \"Install\" &&
+            makepkg -is --noconfirm \" &&
+        find $pkgdir -type f > /tmp/after_install.txt &&
+        diff /tmp/before_install.txt /tmp/after_install.txt > /tmp/changed.txt &&
+        cd / &&
+        echo \"Update the clamAV database\" &&
+        freshclam &&
+        if [ \"$SCANALL\" = true ]; then
+            echo \"Scan the system\" &&
+            clamscan -r
+            if [ \$? -ne 0 ]; then
+                echo \"clamscan failed, continuing...\"
+            fi
+            echo \"Scan for rootkits\" &&
+            rkhunter
+            if [ \$? -ne 0 ]; then
+                echo \"rkhunter failed, continuing...\"
+            fi
+        else
+            echo \"Scan the system\" &&
+            cat /tmp/changed.txt | clamscan -
+            if [ \$? -ne 0 ]; then
+                echo \"clamscan failed, continuing...\"
+            fi
+            echo \"Scan for rootkits\" &&
+            rkhunter --scan-files=$(cat /tmp/changed.txt | tr '\n' ' ')
+            if [ \$? -ne 0 ]; then
+                echo \"clamscan failed, continuing...\"
+            fi
+        fi
+    "
+}
+
 log "Verbose mode enabled"
 echo "Building package: $PACKAGE_NAME"
 echo "Output directory: $OUTPUT_DIR"
 echo "Keep container: $KEEP_CONTAINER"
-systemctl start docker
-
-systemctl stop docker
+test_docker_service
+run_docker_and_test
